11a483649bc3f7eda9f6a056b2ca02d28b4ef8ef
Evgeniy Dekhtyarev <f-k-87@list.ru>
2023-09-02 23:05:11 +0300

    правки

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(requestLogger);
 
 app.use(cors({
   origin: [
+    'https://movies-fronten.nomoredomainsicu.ru',
     'https://movies-frontend.nomoredomainsicu.ru',
     'http://movies-frontend.nomoredomainsicu.ru',
     'http://localhost:3001',
