0af82982a1a3012579b44885b52affd3d8806325
Evgeniy Dekhtyarev <f-k-87@list.ru>
2023-09-03 12:44:51 +0300

    правки2

--- controllers/users.js
+++ controllers/users.js
@@ -50,7 +50,7 @@ const login = (req, res, next) => {
           // token - наш JWT токен, который мы отправляем
           maxAge: 31536000,
           SameSite: 'none',
-          domain: 'https://movies-frontend.nomoredomainsicu.ru',
+          domain: '.movies-frontend.nomoredomainsicu.ru',
           secure: true,
           httpOnly: true,
         })
